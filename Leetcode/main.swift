d920c7b0d1d054b75e35f0106a1ab3b86274a63a
Dzmitry Sazanovich <dzmitrym@google.com>
2020-06-07 11:56:20 -0700

    Initial Commit

---
+++ Leetcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Leetcode
+//
+//  Created by Dzmitry Sazanovich on 6/7/20.
+//  Copyright © 2020 Dzmitry Sazanovich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
